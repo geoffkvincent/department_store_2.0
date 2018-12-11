2ad663449153098b37d3d9cdcec75c015bed80c2
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-11 08:42:28 -0700

    item controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,5 +1,9 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :update, :destroy]
+  
   def index
+    render json: @department.items
   end
 
   def show
@@ -12,5 +16,19 @@ class Api::ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_department
+    @department = Deparment.find(params[:department_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :qty, :price)
   end
 end

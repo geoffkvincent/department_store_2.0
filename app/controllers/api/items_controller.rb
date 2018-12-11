9c2a74f3f0eb013636a8149193cfd7b5f3efcfc6
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-11 08:48:08 -0700

    item controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,21 +1,35 @@
 class Api::ItemsController < ApplicationController
   before_action :set_department
   before_action :set_item, only: [:show, :update, :destroy]
-  
+
   def index
     render json: @department.items
   end
 
   def show
+    render json: @department.item
   end
 
   def create
+    item = @department.items.new(item_params)
+
+    if item.save
+      render json: item
+    else
+      render json: error.item
+    end
   end
 
   def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: error.@item
+    end
   end
 
   def destroy
+    @item.destroy
   end
 
   private

968a396e7e39c119645f21443970df6164b800a9
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-12 10:27:31 -0700

    render items

--- client/src/components/Department.js
+++ client/src/components/Department.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import Items from './Items'
 
 class Department extends React.Component {
   state = {items: [] }
@@ -19,6 +20,7 @@ class Department extends React.Component {
           {name}
         </Link>
         <p>{description}</p>  
+        <Items />
       </div>
     )
   }

85a98ed0ab36a5fcd36da6259ccf0a1fd15c9c81
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-12 10:06:50 -0700

    render departments

--- client/src/components/Department.js
+++ client/src/components/Department.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 class Department extends React.Component {
   state = {items: [] }
 
   componentDidMount() {
-    
+
   }
 
   render() {
+    const {name, description, id} = this.props
     return(
-      null
+      <div>
+        <Link to={`api/departments/${id}`} >
+          {name}
+        </Link>
+        <p>{description}</p>
+      </div>
     )
   }
 }

9008667c011fb626bdc65e03c0dd4909bec3b151
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-12 13:46:33 -0700

    render items route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Store from './components/Store'
+import Items from './components/Items'
 
 const App = () => (
   <>
     <Switch>
       <Route exact path="/" component={Store}/>
+      <Route exact path="/departments/:id/items" component={Items} />
     </Switch>
   </>
 )

b54c2e62ab9844c359b7101f1f3a9328256f9eb6
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-14 11:01:56 -0700

    item show

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -2,12 +2,14 @@ import React from 'react'
 import axios from 'axios'
 
 class Items extends React.Component {
-  state = {items: [] }
+  state = {items: [], Dep: {} }
 
   componentDidMount() {
     const {id} = this.props.match.params
     axios.get(`/api/departments/${id}/items`)
       .then( res => this.setState({ items: res.data }))
+    axios.get(`/api/departments/${id}`)
+      .then( res => this.setState({}))
   }
 
   render() {

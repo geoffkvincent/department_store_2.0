155423cac1a51eca76f562e562669429dac701ed
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-14 11:58:04 -0700

    item form

--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import axios from 'axios'
+import { Form } from 'semantic-ui-react'
 
 class ItemForm extends React.Component {
   state = {name: '', description: '', qty: '', price: ''}
 
   render() {
+    const { name, description, qty, price} = this.state
     return (
-      null
+      <Form>
+        <Form.Input
+          name='name'
+          placeholder='Item'
+          label='Item'
+          value={name}
+          onChange={this.handleChange}
+        />
+        <Form.Input
+          name='description'
+          placeholder='description'
+          label='Description'
+          value={description}
+          onChange={this.handleChange}
+        />
+        <Form.Input
+          name='qty'
+          placeholder='Quantity'
+          label='qty'
+          value={qty}
+          onChange={this.handleChange}
+        />
+        <Form.Input
+          name='price'
+          placeholder='00.00'
+          label='$'
+          value={price}
+          onChange={this.handleChange}
+        />
+      </Form>
     )
   }
 }

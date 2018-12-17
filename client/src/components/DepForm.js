1bd62c78894f444120cdb4e8fea78ab4c177d4b4
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-16 19:49:24 -0700

    dep edit

--- client/src/components/DepForm.js
+++ client/src/components/DepForm.js
@@ -23,7 +23,7 @@ class DepForm extends React.Component {
     e.preventDefault()
     const { id } = this.props.match.params
     if (id) {
-      axios.put(`/api/departments/${id}`), {...this.state}
+      axios.put((`/api/departments/${id}`), {...this.state})
         .then( res => this.props.history.push(`/departments/${res.data.id}`))
     } else {
       axios.post(('/api/departments'), {...this.state})
